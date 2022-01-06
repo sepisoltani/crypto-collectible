38d759dc50eeac535385cf361c523ce62fd6eb01
sepehr <sph_1996@yahoo.com>
2022-01-06 21:32:00 +0330

    add tests for minting

--- test/test.js
+++ test/test.js
@@ -1,5 +1,9 @@
 const { assert } = require('chai');
 const Color = artifacts.require("Color");
+
+
+require('chai').use(require('chai-as-promised')).should()
+
 contract('Color',async(accounts) => {
     let contract
 
@@ -29,6 +33,23 @@ contract('Color',async(accounts) => {
             assert.notEqual(symbol,"")
             assert.notEqual(symbol,undefined)
             assert.notEqual(symbol,null)
+        })
+
+    })
+
+    describe('minting', async() => {
+
+        it('creates a new token', async() => {
+           const result =  await contract.mint("#FFFFFF")
+           const totalSuply = await contract.totalSupply()
+           assert.equal(totalSuply,1)
+           const event = result.logs[0].args
+           assert.equal(event.tokenId.toNumber(),0,'id is correct')
+           assert.equal(event.from,"0x0000000000000000000000000000000000000000",'from is correct')
+           assert.equal(event.to,accounts[0],'to is correct')
+           //cannot mint same color twice
+           await contract.mint("#FFFFFF").should.be.rejected
+    
         })
 
     })
